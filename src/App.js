11cadf48b78f8abe1c3e4217b4e158254b248284
Billy Leung <billy.h.f.leung@gmail.com>
2021-03-28 12:43:19 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World!
+          Hello World! This is my first react app.
         </p>
         <a
           className="App-link"
